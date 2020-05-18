446938e42cc6dd796b7eafd4f89f0b8412a2870e
elsa chen <snowing222@gmail.com>
2020-05-18 23:22:12 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,8 +11,11 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
+<<<<<<< HEAD
   array[1],array[2]=array[2],array[1]
   array
+=======
+>>>>>>> 0fa6fabc03e39abb0028d0b459bbf27bb969a7a4
 
 end
 
@@ -23,7 +26,11 @@ end
 def kesha_maker(array)
   new_array=[]
   array.each do |x|
+<<<<<<< HEAD
     x[2]="$"
+=======
+    x[2]=="$"
+>>>>>>> 0fa6fabc03e39abb0028d0b459bbf27bb969a7a4
     new_array<<x
   end
   new_array
@@ -41,8 +48,14 @@ def add_s(array)
   array.each_with_index.collect do |element,index|
     if index!=1
       "#{element}s"
+<<<<<<< HEAD
     else
       element
     end
+=======
+    end
+
+
+>>>>>>> 0fa6fabc03e39abb0028d0b459bbf27bb969a7a4
   end
 end
